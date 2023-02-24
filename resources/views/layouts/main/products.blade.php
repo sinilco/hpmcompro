1d8587c5277895e5f136d84780fd2f9689f1f41b
Rivaldy Septyasurya <rivaldy.septyasurya@gmail.com>
2023-02-25 00:12:58 +0700

    Halaman products

--- resources/views/layouts/main/products.blade.php
+++ resources/views/layouts/main/products.blade.php
@@ -16,12 +16,133 @@
 @section('product')
     <section class="sample-page">
       <div class="container">
-
-        <p>
-          KOLOM LIST PRODUCTS
-        </p>
-
+        <div class="section-header text-center">
+            <h2>Visit Our Products</h2>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p class="text-center">
+                    Our existing customers have chosen us a partner because of our edge in quality, innovation, reliability, services and reasonable prices. Rest assured, we perform severe quality inspections and tests throughout the manufacturing process. Currently we provide thousands of profiles with various diameter and thickness.
+                    Feel free to download our company profile and preview our brief catalog below, or request our team for send the full version of the catalog especially for you via E-mail.
+                </p>
+                <br><br>
+            </div>
+        </div>
       </div>
+      <section id="projects" class="projects" style="padding: 20px 0">
+        <div class="container" data-aos="fade-up">
+
+          <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry" data-portfolio-sort="original-order">
+
+            <div class="row gy-2 portfolio-container">
+
+                <div class="col-lg-3 col-md-6 portfolio-item filter-active py-2" data-aos="fade-right" data-aos-delay="300">
+                    <div class="portfolio-content h-100">
+                    <img src="https://hpmindonesia.co.id/assets/images/product/product-1.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+
+                        <a href="https://hpmindonesia.co.id/assets/images/product/product-1.jpg" style="left: calc(50% - 55px);" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link">Product A</a>
+                        {{-- <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> --}}
+                    </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <div class="col-lg-3 col-md-6 portfolio-item filter-construction py-2" data-aos="fade-right" data-aos-delay="400">
+                    <div class="portfolio-content h-100">
+                    <img src="https://hpmindonesia.co.id/assets/images/product/product-2.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <a href="https://hpmindonesia.co.id/assets/images/product/product-2.jpg" style="left: calc(50% - 55px);" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link">Product B</a>
+                        <!-- <a href="theme/img/projects/construction-1.jpg" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                        <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                    </div>
+                    </div>
+                </div>
+                <!-- End Projects Item -->
+
+                <div class="col-lg-3 col-md-6 portfolio-item filter-construction py-2" data-aos="fade-left" data-aos-delay="500">
+                    <div class="portfolio-content h-100">
+                    <img src="https://hpmindonesia.co.id/assets/images/product/product-3.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <a href="https://hpmindonesia.co.id/assets/images/product/product-3.jpg" style="left: calc(50% - 55px);" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link">Product C</a>
+                        <!-- <a href="theme/img/projects/construction-1.jpg" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                        <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                    </div>
+                    </div>
+                </div>
+                <!-- End Projects Item -->
+
+                <div class="col-lg-3 col-md-6 portfolio-item filter-construction py-2" data-aos="fade-left" data-aos-delay="600">
+                    <div class="portfolio-content h-100">
+                    <img src="https://hpmindonesia.co.id/assets/images/product/product-4.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <a href="https://hpmindonesia.co.id/assets/images/product/product-4.jpg" style="left: calc(50% - 55px);" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link">Product D</a>
+                        <!-- <a href="theme/img/projects/construction-1.jpg" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                        <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                    </div>
+                    </div>
+                </div>
+                <!-- End Projects Item -->
+
+                <div class="col-lg-3 col-md-6 portfolio-item filter-active py-2" data-aos="fade-right" data-aos-delay="300">
+                    <div class="portfolio-content h-100">
+                    <img src="https://hpmindonesia.co.id/assets/images/product/product-5.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+
+                        <a href="https://hpmindonesia.co.id/assets/images/product/product-5.jpg" style="left: calc(50% - 55px);" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link">Product E</a>
+                        {{-- <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> --}}
+                    </div>
+                    </div>
+                </div><!-- End Projects Item -->
+
+                <div class="col-lg-3 col-md-6 portfolio-item filter-construction py-2" data-aos="fade-right" data-aos-delay="400">
+                    <div class="portfolio-content h-100">
+                    <img src="https://hpmindonesia.co.id/assets/images/product/product-6.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <a href="https://hpmindonesia.co.id/assets/images/product/product-6.jpg" style="left: calc(50% - 55px);" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link">Product F</a>
+                        <!-- <a href="theme/img/projects/construction-1.jpg" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                        <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                    </div>
+                    </div>
+                </div>
+                <!-- End Projects Item -->
+
+                <div class="col-lg-3 col-md-6 portfolio-item filter-construction py-2" data-aos="fade-left" data-aos-delay="500">
+                    <div class="portfolio-content h-100">
+                    <img src="https://hpmindonesia.co.id/assets/images/product/product-7.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <a href="https://hpmindonesia.co.id/assets/images/product/product-7.jpg" style="left: calc(50% - 55px);" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link">Product G</a>
+                        <!-- <a href="theme/img/projects/construction-1.jpg" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                        <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                    </div>
+                    </div>
+                </div>
+                <!-- End Projects Item -->
+
+                <div class="col-lg-3 col-md-6 portfolio-item filter-construction py-2" data-aos="fade-left" data-aos-delay="600">
+                    <div class="portfolio-content h-100">
+                    <img src="https://hpmindonesia.co.id/assets/images/product/product-8.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <a href="https://hpmindonesia.co.id/assets/images/product/product-8.jpg" style="left: calc(50% - 55px);" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link">Product H</a>
+                        <!-- <a href="theme/img/projects/construction-1.jpg" title="Construction 1" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                        <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                    </div>
+                    </div>
+                </div>
+                <!-- End Projects Item -->
+
+            </div>
+        <!-- End Projects Container -->
+
+            <div class="row">
+                <div class="ml-auto text-center">
+                    <br><br>
+                    <a href="https://hpmindonesia.co.id/download-product-catalog"><img style="border:1px solid black" src="https://hpmindonesia.co.id/assets/images/pdf/product_catalog.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  </section>
     </section>
 
   </main><!-- End #main -->
